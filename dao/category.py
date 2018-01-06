f43ea4f0229a646c2c3f2db2ac547135751413cc
j-albarran <josue.albarran@upr.edu>
2018-01-06 17:44:40 -0400

    Add DAO for Category entity
    Signed-off-by: j-albarran <josue.albarran@upr.edu>

--- dao/category.py
+++ dao/category.py
@@ -1,4 +1,5 @@
-
+from config.dbconfig import pg_config
+import psycopg2
 
 class CategoryDAO:
 
@@ -26,45 +27,86 @@ class CategoryDAO:
             result.append(row)
         return result
 
-    ***def insert(self, cat_name):
+    def insert(self, cat_name, cat_pname):
         cursor = self.conn.cursor()
-        query = "insert into Category(cat_name) values (%s);"
+        query = "insert into Category(cat_name, cat_pname) values (%s, %s);"
+        cursor.execute(query, (cat_name, cat_pname))
+        self.conn.commit()
+        return cat_name
+
+    def updateCategory(self, cat_name, cat_pname):
+        cursor = self.conn.cursor()
+        query = "update Category set cat_pname = %s where cat_name = %s;"
+        cursor.execute(query, (cat_pname, cat_name))
+        self.conn.commit()
+        return cat_name
+
+    def delete(self, cat_name):
+        cursor = self.conn.cursor()
+        query = "delete from Category where cat_name = %s;"
         cursor.execute(query, (cat_name,))
-        kid = cursor.fetchone()[0]
         self.conn.commit()
-        return kid
-
-    ***def updateCategory(self, cat_name):
-        cursor = self.conn.cursor()
-        query = "update Keyword set keyword = %s where kid = %s;"
-        cursor.execute(query, (keyword, kid))
-        self.conn.commit()
-        return kid
-
-    ***def delete(self, cat_name):
-        cursor = self.conn.cursor()
-        query = "delete from Keyword where kid = %s;"
-        cursor.execute(query, (kid,))
-        self.conn.commit()
-        return kid
+        return cat_name
 
     def getCategoriesByCityName(self, cname):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from (Category natural inner join Resource natural inner join Supplier) as S inner join Account as A on S.sid = A.aid natural inner join Address where cname = %s;"
+        cursor.execute(query, (cname, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getCategoriesByRegionName(self, rname):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category natural inner join Resource natural inner join Supplier) as S inner join Account as A on S.sid = A.aid natural inner join Address natural inner join City where rname = %s;"
+        cursor.execute(query, (rname, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getCategoriesByResourceId(self, rsid):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category natural inner join Resource where rsid = %s;"
+        cursor.execute(query, (rsid, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getCategoriesByKeywordId(self, kid):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category natural inner join Resource natural inner join ResourceHasKeyword where kid = %s;"
+        cursor.execute(query, (kid, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getCategoriesByResourceRequestedId(self, rrid):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category natural inner join Resource_Requested where rrid = %s;"
+        cursor.execute(query, (rrid, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
-    def getCategoriesByCategoryName(self, cat_name):
-        pass
+    def getCategoriesByCategoryName(self, cat_pname):
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category where cat_pname = %s;"
+        cursor.execute(query, (cat_pname, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
 
     def getCategoriesByTransactionId(self, tid):
-        pass
+        cursor = self.conn.cursor()
+        query = "Select cat_name, cat_pname from Category natural inner join Resource natural inner join Transaction where tid = %s;"
+        cursor.execute(query, (tid, ))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
