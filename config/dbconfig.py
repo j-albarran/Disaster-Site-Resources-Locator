c279205855589125e42d4b85476e7c4761705b49
j-albarran <josue.albarran@upr.edu>
2018-01-08 14:44:28 -0400

    Update DB configuration file to connect to Heroku DB
    Changed connection parameters to establish a connection with
    our Heroku PostgreSQL Database.
    
    Signed-off-by: j-albarran <josue.albarran@upr.edu>

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,7 +1,7 @@
 #Database configuration information
 
 import psycopg2
-import urllib.parse as urlparse
+'''import urllib.parse as urlparse
 import os
 
 url = urlparse.urlparse(os.environ['DATABASE_URL'])
@@ -9,12 +9,20 @@ dbname = url.path[1:]
 user = url.username
 password = url.password
 host = url.hostname
-port = url.port
+port = url.port'''
+
+dbname = 'd3efu32or6poc9'
+user = 'wwoiuehjrodcic'
+password = '9b88e61f237fce06035bde4baf4299e6c1134cabbcc20429c8030e67ce97d235'
+host = 'ec2-107-21-201-57.compute-1.amazonaws.com'
+port = '5432'
+sslmode = 'require'
 
 conn = psycopg2.connect(
             dbname=dbname,
             user=user,
             password=password,
             host=host,
-            port=port
+            port=port,
+            sslmode=sslmode
             )
